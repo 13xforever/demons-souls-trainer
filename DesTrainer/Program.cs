45ac93b768e6aa5fe46b15cfba31b3d69418f6f4
13xforever <ilya.veselov@gmail.com>
2018-11-11 18:14:49 +0500

    Add project files.

---
+++ DesTrainer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesTrainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
