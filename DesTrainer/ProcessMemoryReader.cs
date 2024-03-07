350a002e4accf4591974da2b0df7a0e093759367
13xforever <ilya.veselov@gmail.com>
2024-03-07 17:00:18 +0500

    upgrade to .net 8

--- DesTrainer/ProcessMemoryReader.cs
+++ DesTrainer/ProcessMemoryReader.cs
@@ -1,72 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Runtime.InteropServices;
 
-namespace DesTrainer
+namespace DesTrainer;
+
+public class ProcessMemoryReader: IDisposable
 {
-    public class ProcessMemoryReader: IDisposable
+    private IntPtr procHandle = IntPtr.Zero;
+
+    public static ProcessMemoryReader OpenProcess(Process process)
     {
-        private IntPtr procHandle = IntPtr.Zero;
+        var access = ProcessAccessType.PROCESS_VM_READ |
+                     ProcessAccessType.PROCESS_VM_WRITE |
+                     ProcessAccessType.PROCESS_VM_OPERATION;
+        return new() { procHandle = Kernel32.OpenProcess((uint)access, 1, (uint)process.Id) };
+    }
 
-        public static ProcessMemoryReader OpenProcess(Process process)
-        {
-            var access = ProcessAccessType.PROCESS_VM_READ |
-                         ProcessAccessType.PROCESS_VM_WRITE |
-                         ProcessAccessType.PROCESS_VM_OPERATION;
-            return new ProcessMemoryReader { procHandle = Kernel32.OpenProcess((uint)access, 1, (uint)process.Id) };
-        }
+    public void ReadProcessMemory(IntPtr address, uint bytesToRead, byte[] buffer, out int bytesRead)
+    {
+        var changedProtection = Kernel32.VirtualProtectEx(procHandle, address, (UIntPtr)bytesToRead, MemoryProtection.PAGE_EXECUTE_READWRITE, out var originalProtection);
+        Kernel32.ReadProcessMemory(procHandle, address, buffer, bytesToRead, out var ptrBytesRead);
+        if (changedProtection)
+            Kernel32.VirtualProtectEx(procHandle, address, (UIntPtr)bytesToRead, originalProtection, out _);
+        bytesRead = ptrBytesRead.ToInt32();
+    }
 
-        public void ReadProcessMemory(IntPtr address, uint bytesToRead, byte[] buffer, out int bytesRead)
-        {
-            var changedProtection = Kernel32.VirtualProtectEx(procHandle, address, (UIntPtr)bytesToRead, MemoryProtection.PAGE_EXECUTE_READWRITE, out var originalProtection);
-            Kernel32.ReadProcessMemory(procHandle, address, buffer, bytesToRead, out var ptrBytesRead);
-            if (changedProtection)
-                Kernel32.VirtualProtectEx(procHandle, address, (UIntPtr)bytesToRead, originalProtection, out _);
-            bytesRead = ptrBytesRead.ToInt32();
-        }
+    public void WriteProcessMemory(IntPtr address, byte[] bytesToWrite, out int bytesWritten)
+    {
+        Kernel32.WriteProcessMemory(procHandle, address, bytesToWrite, (uint)bytesToWrite.Length, out var ptrBytesWritten);
+        bytesWritten = ptrBytesWritten.ToInt32();
+    }
 
-        public void WriteProcessMemory(IntPtr address, byte[] bytesToWrite, out int bytesWritten)
+    public List<(ulong offset, ulong length)> GetMemoryRegions()
+    {
+        var result = new List<(ulong offset, ulong length)>();
+        var regions = new HashSet<(ulong offset, ulong length)>();
+        int queryResult;
+        ulong offset = 0;
+        do
         {
-            Kernel32.WriteProcessMemory(procHandle, address, bytesToWrite, (uint)bytesToWrite.Length, out var ptrBytesWritten);
-            bytesWritten = ptrBytesWritten.ToInt32();
-        }
-
-        public List<(ulong offset, ulong length)> GetMemoryRegions()
-        {
-            var result = new List<(ulong offset, ulong length)>();
-            var regions = new HashSet<(ulong offset, ulong length)>();
-            int queryResult;
-            ulong offset = 0;
-            do
+            queryResult = Kernel32.VirtualQueryEx(procHandle, (IntPtr)offset, out var memInfo, (uint)Marshal.SizeOf(typeof(MEMORY_BASIC_INFORMATION)));
+            var allocationBase = (ulong)memInfo.AllocationBase;
+            var baseAddress = (ulong)memInfo.BaseAddress;
+            var regionSize = (ulong)memInfo.RegionSize;
+            var newOffset = baseAddress + regionSize;
+            if (newOffset > offset)
             {
-                queryResult = Kernel32.VirtualQueryEx(procHandle, (IntPtr)offset, out var memInfo, (uint)Marshal.SizeOf(typeof(MEMORY_BASIC_INFORMATION)));
-                var allocationBase = (ulong)memInfo.AllocationBase;
-                var baseAddress = (ulong)memInfo.BaseAddress;
-                var regionSize = (ulong)memInfo.RegionSize;
-                var newOffset = baseAddress + regionSize;
-                if (newOffset > offset)
+                offset = newOffset;
+                if (queryResult > 0)
                 {
-                    offset = newOffset;
-                    if (queryResult > 0)
-                    {
-                        var newRegion = (baseAddress, regionSize);
-                        result.Add(newRegion);
-                    }
+                    var newRegion = (baseAddress, regionSize);
+                    result.Add(newRegion);
                 }
-                else if (baseAddress > 0)
-                    offset += 4096;
-                else
-                    break;
-            } while (queryResult != 0);
-            return result;
-        }
+            }
+            else if (baseAddress > 0)
+                offset += 4096;
+            else
+                break;
+        } while (queryResult != 0);
+        return result;
+    }
 
-        public void Dispose()
-        {
-            if (procHandle != IntPtr.Zero)
-                Kernel32.CloseHandle(procHandle);
-        }
+    public void Dispose()
+    {
+        if (procHandle != IntPtr.Zero)
+            Kernel32.CloseHandle(procHandle);
     }
 }
--- DesTrainer/Program.cs
+++ DesTrainer/Program.cs
@@ -8,135 +8,134 @@ using System.Text;
 using System.Threading;
 using BitConverter;
 
-namespace DesTrainer
+namespace DesTrainer;
+
+static class Program
 {
-    static class Program
+    const int valueLength = 2*3*4;
+    private static readonly ArrayPool<byte> ArrayPool = ArrayPool<byte>.Create(valueLength, 64);
+
+    static void Main(string[] args)
     {
-        const int valueLength = 2*3*4;
-        private static readonly ArrayPool<byte> ArrayPool = ArrayPool<byte>.Create(valueLength, 64);
+        AdjustPrivileges();
 
-        static void Main(string[] args)
+        Console.Clear();
+        Console.WindowWidth = 50;
+        Console.WindowHeight = 5;
+        Console.BufferWidth = Console.WindowWidth;
+        Console.BufferHeight = Console.WindowHeight;
+        do
         {
-            AdjustPrivileges();
-
-            Console.Clear();
-            Console.WindowWidth = 50;
-            Console.WindowHeight = 5;
-            Console.BufferWidth = Console.WindowWidth;
-            Console.BufferHeight = Console.WindowHeight;
-            do
+            Console.Title = "Demon's Souls Trainer";
+            Console.CursorLeft = 0;
+            Console.Write("Searching for the process...");
+            var procList = Process.GetProcesses()
+                .Where(p => p.MainWindowTitle.Contains("Demon's Souls", StringComparison.InvariantCultureIgnoreCase) &&
+                            p.MainModule.ModuleName.Contains("rpcs3", StringComparison.InvariantCultureIgnoreCase)
+                ).ToList();
+            if (procList.Count == 1)
             {
-                Console.Title = "Demon's Souls Trainer";
-                Console.CursorLeft = 0;
-                Console.Write("Searching for the process...");
-                var procList = Process.GetProcesses()
-                    .Where(p => p.MainWindowTitle.Contains("Demon's Souls", StringComparison.InvariantCultureIgnoreCase) &&
-                                p.MainModule.ModuleName.Contains("rpcs3", StringComparison.InvariantCultureIgnoreCase)
-                    ).ToList();
-                if (procList.Count == 1)
+                var des = procList[0];
+                Console.Clear();
+                Console.WriteLine($"Opened process {des.Id}: {des.MainModule.ModuleName}");
+                Console.Title += " (Active)";
+                Console.CursorVisible = false;
+                var ptrBuf = ArrayPool.Rent(4);
+                var valBuf = ArrayPool.Rent(valueLength);
+                var nameBuf = ArrayPool.Rent(2*16);
+                using (var pmr = ProcessMemoryReader.OpenProcess(des))
                 {
-                    var des = procList[0];
-                    Console.Clear();
-                    Console.WriteLine($"Opened process {des.Id}: {des.MainModule.ModuleName}");
-                    Console.Title += " (Active)";
-                    Console.CursorVisible = false;
-                    var ptrBuf = ArrayPool.Rent(4);
-                    var valBuf = ArrayPool.Rent(valueLength);
-                    var nameBuf = ArrayPool.Rent(2*16);
-                    using (var pmr = ProcessMemoryReader.OpenProcess(des))
+
+                    var rpcs3Base = pmr.GetMemoryRegions().First(r => r.offset >= 0x1_0000_0000 && (r.offset % 0x1000_0000 == 0)).offset; // should be either 0x1_0000_0000 or 0x3_0000_0000
+                    Console.WriteLine($"Guest memory base: 0x{rpcs3Base:x8}");
+                    const int statsPointer  = 0x01B4A5EC; // 4
+                    const int characterName = 0x202E80B0; // 16*2
+                    const int currentSouls  = 0x202E8098; // 4
+                    const int offsetHp = 0x3c4;
+                    var pBase = (IntPtr)(rpcs3Base + statsPointer);
+                    do
                     {
-
-                        var rpcs3Base = pmr.GetMemoryRegions().First(r => r.offset >= 0x1_0000_0000 && (r.offset % 0x1000_0000 == 0)).offset; // should be either 0x1_0000_0000 or 0x3_0000_0000
-                        Console.WriteLine($"Guest memory base: 0x{rpcs3Base:x8}");
-                        const int statsPointer  = 0x01B4A5EC; // 4
-                        const int characterName = 0x202E80B0; // 16*2
-                        const int currentSouls  = 0x202E8098; // 4
-                        const int offsetHp = 0x3c4;
-                        var pBase = (IntPtr)(rpcs3Base + statsPointer);
-                        do
+                        pmr.ReadProcessMemory(pBase, 4, ptrBuf, out var readBytes);
+                        if (readBytes == 4)
                         {
-                            pmr.ReadProcessMemory(pBase, 4, ptrBuf, out var readBytes);
-                            if (readBytes == 4)
+                            var ptr = (IntPtr)(rpcs3Base + EndianBitConverter.BigEndian.ToUInt32(ptrBuf, 0) + offsetHp);
+                            pmr.ReadProcessMemory(ptr, valueLength, valBuf, out readBytes);
+                            if (readBytes == valueLength)
                             {
-                                var ptr = (IntPtr)(rpcs3Base + EndianBitConverter.BigEndian.ToUInt32(ptrBuf, 0) + offsetHp);
-                                pmr.ReadProcessMemory(ptr, valueLength, valBuf, out readBytes);
-                                if (readBytes == valueLength)
+                                var hp = EndianBitConverter.BigEndian.ToUInt32(valBuf, 4);
+                                var mp = EndianBitConverter.BigEndian.ToUInt32(valBuf, 12);
+                                var st = EndianBitConverter.BigEndian.ToUInt32(valBuf, 20);
+                                if (hp < 9999 && mp < 9999 & st < 9999)
                                 {
-                                    var hp = EndianBitConverter.BigEndian.ToUInt32(valBuf, 4);
-                                    var mp = EndianBitConverter.BigEndian.ToUInt32(valBuf, 12);
-                                    var st = EndianBitConverter.BigEndian.ToUInt32(valBuf, 20);
-                                    if (hp < 9999 && mp < 9999 & st < 9999)
-                                    {
-                                        Buffer.BlockCopy(valBuf, 4, valBuf, 0, 4);
-                                        Buffer.BlockCopy(valBuf, 12, valBuf, 8, 4);
-                                        Buffer.BlockCopy(valBuf, 20, valBuf, 16, 4);
-                                        pmr.WriteProcessMemory(ptr, valBuf, out _);
-                                        pmr.ReadProcessMemory((IntPtr)(rpcs3Base + characterName), 2 * 16, nameBuf, out readBytes);
+                                    Buffer.BlockCopy(valBuf, 4, valBuf, 0, 4);
+                                    Buffer.BlockCopy(valBuf, 12, valBuf, 8, 4);
+                                    Buffer.BlockCopy(valBuf, 20, valBuf, 16, 4);
+                                    pmr.WriteProcessMemory(ptr, valBuf, out _);
+                                    pmr.ReadProcessMemory((IntPtr)(rpcs3Base + characterName), 2 * 16, nameBuf, out readBytes);
 
-                                        var name = readBytes > 0 ? Encoding.BigEndianUnicode.GetString(nameBuf, 0, readBytes / 2) : "";
-                                        name = name.TrimEnd('\0', ' ');
-                                        if (!string.IsNullOrEmpty(name))
-                                            name += " ";
+                                    var name = readBytes > 0 ? Encoding.BigEndianUnicode.GetString(nameBuf, 0, readBytes / 2) : "";
+                                    name = name.TrimEnd('\0', ' ');
+                                    if (!string.IsNullOrEmpty(name))
+                                        name += " ";
 
-                                        pmr.ReadProcessMemory((IntPtr)(rpcs3Base + currentSouls), 4, ptrBuf, out readBytes);
-                                        var souls = rpcs3Base == 0x1_0000_0000 && readBytes == 4 ? EndianBitConverter.BigEndian.ToInt32(ptrBuf, 0).ToString() : "";
+                                    pmr.ReadProcessMemory((IntPtr)(rpcs3Base + currentSouls), 4, ptrBuf, out readBytes);
+                                    var souls = rpcs3Base == 0x1_0000_0000 && readBytes == 4 ? EndianBitConverter.BigEndian.ToInt32(ptrBuf, 0).ToString() : "";
 
-                                        Console.CursorLeft = 0;
-                                        Console.Write(name);
-                                        Console.ForegroundColor = ConsoleColor.Red;
-                                        Console.Write($"{hp} ");
-                                        Console.ForegroundColor = ConsoleColor.Blue;
-                                        Console.Write($"{mp} ");
-                                        Console.ForegroundColor = ConsoleColor.Green;
-                                        Console.Write($"{st} ");
-                                        Console.ResetColor();
-                                        Console.Write($"{souls}       ");
-                                    }
+                                    Console.CursorLeft = 0;
+                                    Console.Write(name);
+                                    Console.ForegroundColor = ConsoleColor.Red;
+                                    Console.Write($"{hp} ");
+                                    Console.ForegroundColor = ConsoleColor.Blue;
+                                    Console.Write($"{mp} ");
+                                    Console.ForegroundColor = ConsoleColor.Green;
+                                    Console.Write($"{st} ");
+                                    Console.ResetColor();
+                                    Console.Write($"{souls}       ");
                                 }
                             }
+                        }
 #if DEBUG
-                            else
-                            {
-                                var error = Marshal.GetLastWin32Error();
-                                var msg = new Win32Exception(error).Message;
-                                Console.CursorLeft = 0;
-                                Console.Write(msg);
-                            }
+                        else
+                        {
+                            var error = Marshal.GetLastWin32Error();
+                            var msg = new Win32Exception(error).Message;
+                            Console.CursorLeft = 0;
+                            Console.Write(msg);
+                        }
 #endif
-                            Thread.Sleep(100);
-                        } while (!des.HasExited);
-                    }
-                    ArrayPool.Return(nameBuf);
-                    ArrayPool.Return(valBuf);
-                    ArrayPool.Return(ptrBuf);
-                    Console.Clear();
-                    Console.CursorVisible = false;
+                        Thread.Sleep(100);
+                    } while (!des.HasExited);
                 }
-                else
-                    Thread.Sleep(1000);
-            } while (true);
-        }
+                ArrayPool.Return(nameBuf);
+                ArrayPool.Return(valBuf);
+                ArrayPool.Return(ptrBuf);
+                Console.Clear();
+                Console.CursorVisible = false;
+            }
+            else
+                Thread.Sleep(1000);
+        } while (true);
+    }
 
-        private static void AdjustPrivileges()
+    private static void AdjustPrivileges()
+    {
+        try
         {
-            try
-            {
-                TOKEN_PRIVILEGES tp;
-                var hproc = Kernel32.GetCurrentProcess();
-                var htok = IntPtr.Zero;
-                Advapi32.OpenProcessToken(hproc, TokenPriveleges.TOKEN_ADJUST_PRIVILEGES | TokenPriveleges.TOKEN_QUERY, ref htok);
-                tp.PrivilegeCount = 1;
-                tp.Luid = new LUID();
-                tp.Attributes = Advapi32.SE_PRIVILEGE_ENABLED;
-                if (!Advapi32.LookupPrivilegeValue(null, SecurityEntity.SE_DEBUG_NAME, ref tp.Luid))
-                    throw new UnauthorizedAccessException();
+            TOKEN_PRIVILEGES tp;
+            var hproc = Kernel32.GetCurrentProcess();
+            var htok = IntPtr.Zero;
+            Advapi32.OpenProcessToken(hproc, TokenPriveleges.TOKEN_ADJUST_PRIVILEGES | TokenPriveleges.TOKEN_QUERY, ref htok);
+            tp.PrivilegeCount = 1;
+            tp.Luid = new();
+            tp.Attributes = Advapi32.SE_PRIVILEGE_ENABLED;
+            if (!Advapi32.LookupPrivilegeValue(null, SecurityEntity.SE_DEBUG_NAME, ref tp.Luid))
+                throw new UnauthorizedAccessException();
 
-                Advapi32.AdjustTokenPrivileges(htok, false, ref tp, 0, IntPtr.Zero, IntPtr.Zero);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+            Advapi32.AdjustTokenPrivileges(htok, false, ref tp, 0, IntPtr.Zero, IntPtr.Zero);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
         }
     }
 }
